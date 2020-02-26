baefef6532594df4737a92a41ee07a82bfaeb0fe
Junaid <junaidkabani3@gmail.com>
2020-02-25 18:37:55 -0600

    Change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,13 +93,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Opensource.builders`,
+        short_name: `Opensource.builders`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/favicon.png`,
       },
     },
     `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -97,7 +97,7 @@ const Index = ({ data, location }) => {
   })
   return (
     <>
-      <SEO title="All comparisons" />
+      <SEO title="Opensource.builders" />
       <Box
         bg="white"
         boxShadow="0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)"

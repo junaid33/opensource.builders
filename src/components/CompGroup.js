8c1ec0fa3bde6baf8da8d54d790bfa7646836a2f
Junaid <junaidkabani3@gmail.com>
2020-06-04 01:15:21 -0500

    Add infinite scroll

--- src/components/CompGroup.js
+++ src/components/CompGroup.js
@@ -35,63 +35,6 @@ export default function CompGroup({ comp, commercial, alts }) {
             </Box>
           ))}
         </Box>
-        {/* <Box display="flex" alignItems="center">
-          <Box>
-            <Box
-              as="img"
-              height="4rem"
-              py={3}
-              px={4}
-              borderTopLeftRadius={5}
-              borderBottomLeftRadius={5}
-              src={comp.svg}
-              alt=""
-            />
-          </Box>
-          <Link
-            role="group"
-            bg="gray.50"
-            borderRadius="4px"
-            py={2}
-            px={3}
-            color="gray.500"
-            display="flex"
-            alignItems="center"
-            ml="auto"
-            _hover={{ textDecoration: "none" }}
-            href={comp.website}
-            target="_blank"
-            fontWeight={500}
-            fontSize={15}
-            mr={1}
-          >
-            <PseudoBox
-              fontWeight={500}
-              fontSize={15}
-              mr={2}
-              transition="all 0.15s ease-out"
-              _groupHover={{ pr: 1 }}
-              display={{ base: "none", sm: "flex" }}
-            >
-              {comp.main}
-            </PseudoBox>
-            <Box size={4}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-arrow-up-right"
-              >
-                <line x1="7" y1="17" x2="17" y2="7"></line>
-                <polyline points="7 7 17 7 17 17"></polyline>
-              </svg>
-            </Box>
-          </Link>
-        </Box> */}
         <Divider borderColor="gray.400" />
         <Box display="flex" flexWrap="wrap">
           {alts.length <= 5 ? (
--- src/templates/index-with-search.js
+++ src/templates/index-with-search.js
@@ -1,16 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
+import { useBottomScrollListener } from "react-bottom-scroll-listener"
+import { Box, Text, Icon } from "@chakra-ui/core"
 import { graphql } from "gatsby"
 import { useLocalJsonForm } from "gatsby-tinacms-json"
 import ClientSearch from "../components/ClientSearch"
 
 const SearchTemplate = props => {
+  const [loadCount, setLoadCount] = useState(6)
+
   const { pageContext } = props
   const { compData } = pageContext
   const { allComps, mainInfo, options } = compData
 
+  const fiveComp = mainInfo.slice(0, loadCount)
+  const restComp = mainInfo.slice(loadCount, mainInfo.length)
+
+  function handleScroll() {
+    if (loadCount < mainInfo.length) {
+      setLoadCount(loadCount + 6)
+    }
+  }
+
+  useBottomScrollListener(() => handleScroll())
+
   return (
     <>
-      <ClientSearch comps={allComps} mainInfo={mainInfo}/>
+      <ClientSearch comps={allComps} mainInfo={fiveComp} />
     </>
   )
 }
